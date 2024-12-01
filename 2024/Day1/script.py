80fbdd0fc0ac4f348181ff35851485b295efb0c7
NCLAN\hahe <hahe@netcompany.com>
2024-12-01 11:22:24 +0100

    Day 1 first challenge

---
+++ 2024/Day1/script.py
@@ -0,0 +1,23 @@
+print("Day1 challenge: start")
+
+leftlist = []
+rightlist = []
+
+with open("input.txt", "r") as file:
+    for line in file:
+        linesplit = line.split()
+        leftlist.append(int(linesplit[0]))
+        rightlist.append(int(linesplit[1]))
+
+#Sort the lists in ascending order
+leftlist.sort()
+rightlist.sort()
+
+totalSum = 0
+
+for index, value in enumerate(leftlist):
+    hej = abs(value - rightlist[index])
+    print(str(value) + " " + str(rightlist[index]) + " " + str(hej))
+    totalSum = totalSum + hej
+
+print("Total distance is: " + str(totalSum))
